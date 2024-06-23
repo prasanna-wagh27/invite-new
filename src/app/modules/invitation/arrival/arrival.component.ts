b825fbb27f3ac5f10536f89874c529a004e1b8f4
Prasanna Wagh <prasannawagh146@gmail.com>
2024-06-23 21:43:55 +0530

    added home component

--- src/app/modules/invitation/arrival/arrival.component.ts
+++ src/app/modules/invitation/arrival/arrival.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-declare var $: any;
 
 @Component({
   selector: 'app-arrival',
@@ -7,53 +6,5 @@ declare var $: any;
   styleUrl: './arrival.component.css'
 })
 export class ArrivalComponent {
-
-  arrivalSection : boolean = true;
-  travelSection : boolean = false;
-  tuesdaySection : boolean = false;
-  wednesdaySection:  boolean = false;
-  thursdayMorningSection: boolean = false;
-  temple = false;
-  starbucks = false;
-
-
-  ngOnInit(){
-
-  }
-
-selectNo() {
-  console.log('ok');
-  $('#warningModal').modal('show');
-}
-
-modalHide(arg0: string) {
-  $('#warningModal').modal('hide');
-}
-
-selectYes(){
-  this.arrivalSection = false;
-  this.travelSection = true;
-}
-
-selectBus(){
-  this.travelSection = false;
-  this.tuesdaySection = true;
-}
-
-selectTuesdayPlan( string : String){
-  this.tuesdaySection = false;
-  this.wednesdaySection = true;
-}
-
-selectWednesdayPlan(){
-  this.wednesdaySection = false;
-  this.thursdayMorningSection = true;
-  this.temple = true;
-}
-
-selectThursdayMorningPlan(){
-  this.temple = false;
-  this.starbucks = true;
-}
 
 }
--- src/app/modules/invitation/homepage/homepage.component.ts
+++ src/app/modules/invitation/homepage/homepage.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'app-homepage',
@@ -6,5 +7,85 @@ import { Component } from '@angular/core';
   styleUrl: './homepage.component.css'
 })
 export class HomepageComponent {
+
+  
+
+  arrivalSection : boolean = true;
+  travelSection : boolean = false;
+  tuesdaySection : boolean = false;
+  wednesdaySection:  boolean = false;
+  thursdayMorningSection: boolean = false;
+  temple = false;
+  starbucks = false;
+  thursdayDaySection: boolean = false;
+  alternatePlan: boolean = false;
+  final: boolean = false;
+  mahabaleshwar: boolean = true;
+
+  data = { tuesdayPlan: '', wednesdayPlan: '', thursdayMorningPlan: '', thursdayDayPlan: '', alternatePlan: ''}
+
+  ngOnInit(){
+
+  }
+
+selectNo() {
+  console.log('ok');
+  $('#warningModal').modal('show');
+}
+
+modalHide(arg0: string) {
+  $('#warningModal').modal('hide');
+}
+
+selectYes(){
+  this.arrivalSection = false;
+  this.travelSection = true;
+}
+
+selectBus(){
+  this.travelSection = false;
+  this.tuesdaySection = true;
+}
+
+selectTuesdayPlan(plan: string){
+  this.data.tuesdayPlan = plan;
+  this.tuesdaySection = false;
+  this.wednesdaySection = true;
+}
+
+selectWednesdayPlan(){
+  this.wednesdaySection = false;
+  this.thursdayMorningSection = true;
+  this.temple = true;
+}
+
+selectThursdayMorningPlan(){
+  this.temple = false;
+  this.starbucks = true;
+}
+
+selectThursdayMorningPlanTwo(){
+  this.starbucks = false;
+  this.thursdayMorningSection = false;
+  this.thursdayDaySection = true;
+}
+
+selectThursdayPlan(plan : string){
+  this.data.thursdayDayPlan = plan;
+  this.thursdayDaySection = false;
+  this.alternatePlan = true;
+}
+
+hurray(){
+  this.alternatePlan = false;
+  this.final = true;
+  this.mahabaleshwar = true;
+}
+
+ohh(){
+  this.alternatePlan = false;
+  this.final = true;
+  this.mahabaleshwar = false;
+}
 
 }
